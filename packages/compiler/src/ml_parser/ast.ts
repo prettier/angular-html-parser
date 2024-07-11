3d4456fff8454b973aaf8df3e7f79fed7cba4681
fisker <lionkay@gmail.com>
2024-07-11 21:09:25 +0800

    Fix bad merge

--- packages/compiler/src/ml_parser/ast.ts
+++ packages/compiler/src/ml_parser/ast.ts
@@ -26,7 +26,8 @@ export type Node =
   | ExpansionCase
   | Text
   | Block
-  | BlockParameter;
+  | BlockParameter
+  | LetDeclaration;
 
 export abstract class NodeWithI18n implements BaseNode {
   constructor(
